172849d68498950e7055f48114e9c91de72f4632
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-11-01 23:16:17 +0530

    MusicPlayer: Add skeletal MusicService (MediaBrowserServiceCompat)
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

---
+++ app/src/main/java/com/pk/musicplayer/services/MusicService.java
@@ -0,0 +1,27 @@
+package com.pk.musicplayer.services;
+
+import android.os.Bundle;
+import android.support.v4.media.MediaBrowserCompat;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.media.MediaBrowserServiceCompat;
+
+import java.util.List;
+
+public class MusicService extends MediaBrowserServiceCompat {
+
+
+    // --------------------- Necessary overridden methods --------------------- //
+
+    @Nullable
+    @Override
+    public BrowserRoot onGetRoot(@NonNull String clientPackageName, int clientUid, @Nullable Bundle rootHints) {
+        return null;
+    }
+
+    @Override
+    public void onLoadChildren(@NonNull String parentId, @NonNull Result<List<MediaBrowserCompat.MediaItem>> result) {
+
+    }
+}
